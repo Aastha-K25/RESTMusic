62c7a1c9dcbbc91c61fe1dc858fe0ea249e359a5
Eylem003 <eylem310305@gmail.com>
2026-04-13 17:12:52 +0200

    Testing

--- RESTMusic.Test/UnitTest1.cs
+++ RESTMusic.Test/UnitTest1.cs
@@ -1,9 +1,75 @@
-﻿namespace RESTMusic.Test;
+﻿using Xunit;
+using System.Linq;
+namespace RESTMusic.Test;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void GetAll_ReturnsData()
     {
+        MusicRecordsRepository repo = new MusicRecordsRepository(true); // arrange
+
+        var result = repo.GetAll(); // Act
+        
+        Assert.NotNull(result);
+        Assert.True(result.Count() > 0);
+    }
+
+    [Fact]
+    public void Add_AddsRecord()
+    {
+        MusicRecordsRepository repo = new MusicRecordsRepository();
+        MusicRecord record = new MusicRecord
+        {
+            Title = "Test",
+            Artist = "Test",
+            Duration = 200,
+            PublicationYear = 2021
+        };
+
+        var result = repo.Add(record);
+        
+        Assert.NotNull(result);
+        Assert.Equal("Test", result.Title);
+    }
+
+    [Fact]
+    public void GetById_ReturnsCorrectRecord()
+    {
+        MusicRecordsRepository repo = new MusicRecordsRepository(true);
+
+        var result = repo.GetById(1);
+
+        Assert.NotNull(result);
+        Assert.Equal(1, result.Id);
+    }
+
+    [Fact]
+    public void remove_DeletesRecord()
+    {
+        MusicRecordsRepository repo = new MusicRecordsRepository(true);
+
+        var result = repo.Remove(1);
+
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public void Update_ChangesData()
+    {
+        MusicRecordsRepository repo = new MusicRecordsRepository(true);
+
+        MusicRecord updated = new MusicRecord()
+        {
+            Title = "Updted",
+            Artist = "Updted",
+            Duration = 200,
+            PublicationYear = 2021
+        };
+
+        var result = repo.Update(1, updated);
+
+        Assert.NotNull(result);
+        Assert.Equal("Updted", result.Title);
     }
 }
